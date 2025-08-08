467cafd90370f04d4752836be7998195205ffe37
Pramudithalakshan <pramudithalakshan00@gmail.com>
2025-08-08 08:45:32 +0530

    Initial Commit

---
+++ Example.java
@@ -0,0 +1,34 @@
+class Example{
+ public static void main(String args[]){
+	  System.out.println(" __   ______             __");
+	  System.out.println("|  \\ /      \\           |  \\");
+	  System.out.println(" \\$$|  $$$$$$\\  ______  | $$   ______");
+	  System.out.println("|  \\| $$   \\$$ |      \\ | $$ /       \\");
+	  System.out.println("| $$| $$        \\$$$$$$\\| $$|  $$$$$$$");
+	  System.out.println("| $$| $$   __  /      $$| $$| $$");
+	  System.out.println("| $$| $$__/  \\   $$$$$$$| $$| $$_____");
+	  System.out.println("| $$ \\      $$ \\$$    $$| $$ \\$$     \\");
+	  System.out.println(" \\$$  \\$$$$$$  \\$$$$$$$ \\$$  \\$$$$$$$");
+	  
+	  System.out.println();
+	  System.out.println(" _   _                         _                          _____                                           _");
+	  System.out.println("| \\ | |                       | |                       / _____|                                         | |");
+	  System.out.println("|  \\| |  __   __   _ __ ___   | |__      ___    _ __   | |         ___    _ __   __    __   ___   _ __   | |_    ___    _ __");
+	  System.out.println("| . ` | |  | |  | | '_  `_ \\  | '_ \\   /  _ \\  | *__|  | |        / _ \\  | '_ \\  \\ \\  / /  / _ \\ | '__|  | __|  / _ \\  | '__|");
+	  System.out.println("| |\\  | |  |_|  | | | | | | | | | ) | |  __ /  | |     | |_____  | (_) | | | | |  \\ \\/ /  |  __/ | |     | |_  |  __/  | |");
+	  System.out.println("|_| \\_|  \\__,___| |_| |_| |_| |_.__/   \\____|  |_|      \\______|  \\___/  |_| |_|   \\__/    \\___| |_|      \\__|  \\___|  |_|");
+	  System.out.println();
+	  System.out.println("====================================================================================================================================");
+	  System.out.println();
+	  System.out.println();
+	  
+	  int op1=1,op2=2,op3=3,op4=4,op5=5;
+	  System.out.printf("%5s %d %s %s %n","[",op1,"]","Decimal Converter");
+	  System.out.printf("%5s %d %s %s %n","[",op2,"]","Binary Converter");
+	  System.out.printf("%5s %d %s %s %n","[",op3,"]","Octal Converter");
+	  System.out.printf("%5s %d %s %s %n","[",op4,"]","Hexadecimal Converter");
+	  System.out.printf("%5s %d %s %s %n","[",op5,"]","Roman Converter");
+	
+	
+	 }
+}
