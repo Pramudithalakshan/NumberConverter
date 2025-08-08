23325c08cda170196a46f0d4b5896d3fdea75cfb
Pramudithalakshan <pramudithalakshan00@gmail.com>
2025-08-08 23:33:23 +0530

    Completed converter without roman number converter

--- Example.java
+++ Example.java
@@ -140,7 +140,7 @@ class Example{
 	  System.out.print("Enter an Binary number :");
 	  int binNum = input.nextInt();
       int temp=binNum%10;
-	  if(temp==8 | temp==9 | binNum<0){
+	  if(temp>1 | binNum<0){
 	   System.out.println("Invalid Input");
 	   
 	     System.out.print("Do you want to go to homepase (Y/N)->");
@@ -171,7 +171,7 @@ class Example{
 		}
 		position=0;
 		int octTemp2=deciSt;
-	//Binary to octal
+ 	//Binary to octal
 		 do 
 		 {
 			octSt2=octTemp2%8+octSt2;
@@ -227,8 +227,174 @@ class Example{
       break;
       }
 	  case 3:
+	  L3:while(true){
+	  System.out.println("+-----------------------------------------+");
+	  System.out.println(" |             Octal Converter            |");
+	  System.out.println("+-----------------------------------------+"); 
+	  System.out.println();
+	  System.out.println();
+	  System.out.print("Enter an octal number :");
+	  int octNum = input.nextInt();
+      int temp=octNum%10;
+      if(temp==8 | temp==9 | temp<0){
+		  System.out.println("Invalid Input");
+	   
+	     System.out.print("Do you want to go to homepase (Y/N)->");
+	     String decision = input.next();
+	     int deci =  decision.charAt(0);
+	  	      
+	     if(deci==89|deci==121){
+			  continue L3;
+		 }else{
+		   System.out.print("Exit!");	 
+		 }
+	   }else{
+		   int deciTemp = octNum;
+		   int deciSt=0;
+		   int position=0;
+		   //Octal to decimal
+		   while (deciTemp!=0){
+			int b =deciTemp%10;
+			deciSt+=b*Math.pow(8,position);
+			deciTemp/=10;
+			position++;
+		   }
+		   String binSt="";
+		   int binTemp=deciSt;
+		   //Octal to binary
+		   do{
+			  binSt=binTemp%2+binSt;
+			  binTemp/=2;
+			  
+		  }while(binTemp!=0);
+		  
+		   //Octal to hexadecimal
+		   int hexTemp=deciSt;
+		   String hexSt="";
+	     do 
+		 {
+			
+			int remainder=hexTemp%16;
+	
+			switch(remainder){
+			  case 10:
+			    hexSt="a"
+			    +hexSt;
+			    break;
+			  case 11:
+			    hexSt="b"
+			    +hexSt;
+			    break;
+			  case 12:
+			    hexSt="c"
+			    +hexSt;
+			    break;
+			  case 13:
+			    hexSt="d"
+			    +hexSt;
+			    break;
+			  case 14:
+			    hexSt="e"
+			    +hexSt;
+			    break;
+			  case 15:
+			    hexSt="f"
+			    +hexSt;
+			   	break; 
+			  default:
+			    hexSt=remainder+hexSt;
+			 }	 
+			hexTemp/=16;
+		 
+			
+		 }while(hexTemp!=0);
+		   
+		    System.out.println();
+	      System.out.println("Decimal number :"+deciSt);
+		  System.out.println("binary number :"+binSt);
+		  System.out.println("Hexadecimal number :"+hexSt); 
+	   }
+	     break;
+	  }
+	
 	  case 4:
+	  L4:while(true){
+	  System.out.println("+-----------------------------------------+");
+	  System.out.println("|          Hexadecimal Converter          |");
+	  System.out.println("+-----------------------------------------+"); 
+	  System.out.println();
+	  System.out.println();
+	  
+	  System.out.print("Enter an hexadecimal number :");
+	  String hexNum = input.next();
+      boolean novalid=true;
+    
+	   for (int i = 0; i < hexNum.length(); i++){
+		  char ch = hexNum.charAt(i);
+		  if(!((ch>='0' && ch<='9')||(ch>='A' && ch<='F') ||(ch>='a' && ch<='f') )){	    	 
+		     novalid=false;
+		     break;
+	     }
+	   }
+	   
+		  if(novalid){
+		    		   
+              int deciNum = Integer.parseInt(hexNum,16);
+              String binSt="";
+              int binTemp=deciNum;
+              
+              String octSt="";
+              int octTemp=deciNum;
+           //Hexadecimal to binary
+           do{
+			  binSt=binTemp%2+binSt;
+			  binTemp/=2;
+			  
+		  }while(binTemp!=0);   
+              
+          do{
+			octSt=octTemp%8+octSt;
+			octTemp/=8;
+			
+		 }while(octTemp!=0);    
+              
+          System.out.println();
+	      System.out.println("Decimal number :"+deciNum);
+		  System.out.println("binary number :"+binSt);
+		  System.out.println("Hexadecimal number :"+octSt);
+			  
+		   }else{
+	
+			  
+	     System.out.println("Invalid Input");
+	     System.out.print("Do you want to go to homepase (Y/N)->");
+	     String decision = input.next();
+	     int deci =  decision.charAt(0);
+	  	      
+	         if(deci==89|deci==121){
+			  continue L4;
+		     }else{
+		       System.out.print("Exit!");	 
+		     }
+		     }
+          break;
+	  }
 	  case 5:
-	 }
-	 }
-}
+	  while(true){
+		   L4:while(true){
+	  System.out.println("+-----------------------------------------+");
+	  System.out.println("|          Hexadecimal Converter          |");
+	  System.out.println("+-----------------------------------------+"); 
+	  System.out.println();
+	  System.out.println();
+	  
+	  System.out.print("Enter an hexadecimal number :");
+	  String hexNum = input.next();
+      boolean novalid=true;
+	  }
+  }
+	  
+  
+   }
+ }
+
